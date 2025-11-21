e34cf3758c9dfa1d420cc9407a46f2fad719193f
Janitha Premachandra <146804006+mju-premachandra@users.noreply.github.com>
2025-11-21 14:02:08 +0530

    Delete src directory

--- src/utils/classSchedules.js
+++
@@ -1,9 +0,0 @@
-const classSchedules = {
-  "Studio Dance Core, Shelidian International School, Wava Road, Malabe": {
-    Saturday: [
-      { time: '10:00 AM - 12:00 PM', name: 'Western Dancing', instructor: 'Mr. Jayaruwan' },
-    ]
-  }
-};
-
-export default classSchedules;
